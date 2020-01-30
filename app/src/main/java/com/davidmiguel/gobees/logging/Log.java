53b8bbaf45229234fb6f8550d3a897df5e367ff7
epd1002 <epd1002@users.noreply.github.com>
2020-01-30 15:37:44 +0100

    Add Crashlytics #27

--- app/src/main/java/com/davidmiguel/gobees/logging/Log.java
+++ app/src/main/java/com/davidmiguel/gobees/logging/Log.java
@@ -19,6 +19,7 @@
 
 package com.davidmiguel.gobees.logging;
 
+import com.crashlytics.android.Crashlytics;
 import com.davidmiguel.gobees.BuildConfig;
 
 import timber.log.Timber;
@@ -41,6 +42,25 @@ public class Log {
         if (BuildConfig.DEBUG) {
             Timber.plant(new DebugTree());
         }
+		// Log to Crashlytics
+        Timber.plant(new CrashlyticsTree());
+        // Attach build info
+        setCommitHash(BuildConfig.COMMIT_HASH);
+        setBuildDate(BuildConfig.BUILD_DATE);
+    }
+
+    /**
+     * Attaches commit hash in crash reports.
+     */
+    public static void setCommitHash(String hash) {
+        Crashlytics.setString("commit_hash", hash);
+    }
+
+    /**
+     * Attaches build date in crash reports.
+     */
+    public static void setBuildDate(String date) {
+        Crashlytics.setString("build_date", date);
     }
 
     /**
